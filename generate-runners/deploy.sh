738f4b69c3607ab584bca82d470cee87087a78be
Igor Pecovnik <igor.pecovnik@gmail.com>
2024-03-03 13:26:37 +0100

    Delete from server

--- generate-runners/deploy.sh
+++ generate-runners/deploy.sh
@@ -21,6 +21,41 @@ REPO=os
 # don't edit below
 # -------------------------------------------------------------
 
+runner_delete ()
+{
+	DELETE=$1
+	x=1
+	while [ $x -le 9 ] # need to do it different as it can be more then 9 pages
+	do
+	RUNNER=$(
+	curl -s -L \
+	-H "Accept: application/vnd.github.v3+json" \
+	-H "Authorization: Bearer $TOKEN"\
+	-H "X-GitHub-Api-Version: 2022-11-28" \
+	"https://api.github.com/orgs/${ORG}/actions/runners?page=${x}" | jq -r '.runners[] | .id, .name' | xargs -n2 -d'\n' | sed -e 's/ /,/g'
+	)
+
+	while IFS= read -r DATA; do
+	#echo "Line: $RUNNER_ID"
+	RUNNER_ID=$(echo $DATA | cut -d"," -f1)
+	RUNNER_NAME=$(echo $DATA | cut -d"," -f2)
+	#echo "Line: $RUNNER_ID $RUNNER_NAME"
+
+	# deleting a runner
+	if [[ $RUNNER_NAME == ${DELETE} ]]; then
+	curl -L \
+	-X DELETE \
+	-H "Accept: application/vnd.github+json" \
+	-H "Authorization: Bearer ${TOKEN}"\
+	-H "X-GitHub-Api-Version: 2022-11-28" \
+	https://api.github.com/orgs/${ORG}/actions/runners/${RUNNER_ID}
+	fi
+
+	done <<< $RUNNER
+	x=$(( $x + 1 ))
+done
+}
+
 # we can generate per org or per repo
 REGISTRATION_URL="${ORG}"
 PREFIX="orgs"
@@ -47,6 +82,7 @@ do
 	  https://api.github.com/${PREFIX}/${REGISTRATION_URL}/actions/runners/registration-token | jq -r .token)
 
 	if id "actions-runner-${i}" >/dev/null 2>&1; then
+		runner_delete "$NAME-${i}"
 		runner_home=$(getent passwd "actions-runner-${i}" | cut -d: -f6)
 		sh -c "cd ${runner_home} ; sudo ./svc.sh stop actions-runner-${i}; sudo ./svc.sh uninstall actions-runner-${i}"
 		sudo userdel -r -f actions-runner-${i}
